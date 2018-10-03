b031b40637e47ba2f5f4488030bcf202d4dcf51d
Joshua Cancellier <sonicjcan@hotmail.com>
2018-10-02 22:31:33 -0700

    Add files via upload

---
+++ assignments/AStarSearch/astar.pl
@@ -0,0 +1,66 @@
+%--------------------------------------------------------------%
+%   Informed Graph Searching Algorithms                        %
+%   The graph should be represented as a set of facts          %
+%   arc(Node1,Node2,Cost) loaded in the database               %
+%   (C) 1998 Zdravko Markov                                    %
+%--------------------------------------------------------------%
+
+%--------------------------------------------------------------%
+%   Best-First Search                                          %
+%   call: best_first(+[[Start]],+Goal,-Path,-ExploredNodes).   %
+%--------------------------------------------------------------%
+astar([[Goal|Path]|_],Goal,[Goal|Path],0).
+astar([Path|Queue],Goal,FinalPath,N) :-
+    extend(Path,NewPaths), 
+    append(Queue,NewPaths,Queue1),
+    sort_queue1(Queue1,NewQueue), wrq(NewQueue),
+    astar(NewQueue,Goal,FinalPath,M),
+    N is M+1.
+
+extend([Node|Path],NewPaths) :-
+    findall([NewNode,Node|Path],
+            (arc(Node,NewNode,_), 
+            \+ member(NewNode,Path)), % for avoiding loops
+            NewPaths).
+
+sort_queue1(L,L2) :-
+    swap1(L,L1), !,
+    sort_queue1(L1,L2).
+sort_queue1(L,L).
+
+swap1([[A1|B1],[A2|B2]|T],[[A2|B2],[A1|B1]|T]) :-
+    hh(A1,W1),
+    hh(A2,W2),
+    g([A1|B1], Dx),
+    g([A2|B2], Dy),
+    W1 + Dx > W2 + Dy.
+swap1([X|T],[X|V]) :-
+    swap1(T,V).
+
+%--------------------------------------------------------------%
+%   Heuristic functions                                        %
+%--------------------------------------------------------------%
+
+% check if the heuristic function is ok.
+hh(State, Value) :- 
+    h(State,Value),
+    number(Value), !.
+hh(State, Value) :- 
+   write('Incorrect heuristic functionh: '),
+   write(h(State, Value)), nl,
+   abort.
+
+%--------------------------------------------------------------%
+
+wrq(Q) :- length(Q,N), writeln(N).
+
+g([A, B], Cost) :-
+    arc(B, A, Cost).
+g([A, B|T], Cost) :-
+    % cost from a to b
+    arc(B, A, InterCost),
+    % Tcost <- recursive call
+    g([B|T], Tcost),
+    % cost <- cost from a to b + PLUS Tcost
+    Cost is InterCost + Tcost.
+
